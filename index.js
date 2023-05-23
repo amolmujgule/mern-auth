852336ceedc991a1e224ccfc723da43d7302bebe
amolmujgule <115858166+amolmujgule@users.noreply.github.com>
2023-05-23 10:03:01 +0530

    23-5-2023

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express")
+require("dotenv").config({path:"./.env"})
+const mongoose = require("mongoose")
+// const cors = require("cors")
+mongoose.connect(process.env.MONGO_URL)
+
 const app = express()
 
+app.use(express.json())
+app.use("/api/user", require("./routes/userRoutes")) 
+
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`http://localhost:${PORT}`))
+mongoose.connection.once("open",() => {
+    console.log("DB CONNECTED");
+    app.listen(PORT,console.log(`http://localhost:${PORT}`))
+})
